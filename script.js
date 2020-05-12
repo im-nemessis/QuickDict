2945737d6397211cb4746097c3fd13547196efbb
im-nemessis <powerfulltech15@gmail.com>
2020-05-12 19:46:44 +0530

    intial commit

---
+++ script.js
@@ -0,0 +1,5 @@
+let body = document.querySelector("body");
+let w = window.innerWidth;
+let h = window.innerHeight;
+let canvas = `<canvas style='opacity:.2' width='${w}' height='${h}'></canvas>`;
+body.innerHtml = canvas;
